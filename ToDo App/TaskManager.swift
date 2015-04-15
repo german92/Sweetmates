e76a1809a70805f405af73db1181f721a56f49c7
German Ostaszynski <german92@gmail.com>
2015-04-15 12:38:12 -0400

    first commit

---
+++ ToDo App/TaskManager.swift
@@ -0,0 +1,13 @@
+//
+//  TaskManager.swift
+//  ToDo App
+//
+//  Created by Luke Stephen Rehmann on 1/16/15.
+//  Copyright (c) 2015 Ostaszynski & Rehmann. All rights reserved.
+//
+
+import UIKit
+
+class TaskManager: NSObject {
+   
+}
