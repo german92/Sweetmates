a63dae0f423721c70f3a13ca2f45995a56178ab9
German Ostaszynski <german92@gmail.com>
2015-04-22 17:43:23 -0400

    Buttons now change status

--- ToDo App/StatusSelectViewController.swift
+++ ToDo App/StatusSelectViewController.swift
@@ -11,12 +11,43 @@ import UIKit
 class StatusSelectViewController: UIViewController {
 
     //Base code for status by clicking button
-
+    
+    
+    @IBOutlet weak var currentStatus: UILabel!
     @IBOutlet weak var studyStatus: UIButton!
-
+    @IBOutlet weak var sleepStatus: UIButton!
+    @IBOutlet weak var friendOverStatus: UIButton!
+    @IBOutlet weak var busyStatus: UIButton!
+    @IBOutlet weak var examStatus: UIButton!
+    @IBOutlet weak var availableStatus: UIButton!
+    
     @IBAction func buttonPressed(sender: AnyObject) {
-        println(studyStatus.titleLabel!.text)
+        currentStatus.text = "Studying"
     }
+    
+    @IBAction func changeSleepingStatus(sender: AnyObject) {
+        currentStatus.text = "Sleeping"
+    }
+    
+    
+    @IBAction func changeFriendOverStatus(sender: AnyObject) {
+        currentStatus.text = "Friend(s) Over"
+    }
+    
+    @IBAction func changeBusyStatus(sender: AnyObject) {
+        currentStatus.text = "Busy"
+    }
+    
+    @IBAction func changeExamStatus(sender: AnyObject) {
+        currentStatus.text = "Exam Tomorrow"
+
+    }
+    
+    @IBAction func changeAvailableStatus(sender: AnyObject) {
+          currentStatus.text = "Available"
+    }
+    
+    
     
     //-------------------------------------------
     
